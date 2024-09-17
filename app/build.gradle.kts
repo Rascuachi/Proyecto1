8ee013a5e75547267492a3ddb59288eb4f88bd42
Rascuachi <rascuache26@gmail.com>
2024-09-16 18:09:26 -0600

    HomeScreen and MenuScreen navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {//aqui se agregan las librerias android
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
