2cefad82e759d1ada005a4a23f2b560ffc863bed
Rascuachi <rascuache26@gmail.com>
2024-09-29 19:08:43 -0600

    Add Buttons FloatingButtons and Chips

--- app/src/main/java/com/ita/myapplication/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/ita/myapplication/ui/screens/ComponentsScreen.kt
@@ -1,14 +1,32 @@
 package com.ita.myapplication.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem // Asegúrate de que este import está presente
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,7 +34,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
@@ -28,11 +48,13 @@ fun ComponentsScreen(navController: NavController) {
     val scope = rememberCoroutineScope()
 
     ModalNavigationDrawer(
+
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
                 Text("Menu", modifier = Modifier.padding(16.dp))
                 HorizontalDivider()
+
                 //Content1
                 NavigationDrawerItem(
                     label = {
@@ -45,7 +67,8 @@ fun ComponentsScreen(navController: NavController) {
                             drawerState.close() // Uso correcto para cerrar el Drawer
                         }
                     }
-                )
+                )//end NavigationDrawerItem
+
                 //Content2
                 NavigationDrawerItem(
                     label = {
@@ -58,11 +81,40 @@ fun ComponentsScreen(navController: NavController) {
                             drawerState.close() // Uso correcto para cerrar el Drawer
                         }
                     }
-                )
+                )//end NavigationDrawerItem
 
-            }
-        }
+                //FloatingAction
+                NavigationDrawerItem(
+                    label = {
+                        Text(text = "Floating Buttons")
+                    },
+                    selected = false,
+                    onClick = {
+                        component = "floating-buttons"
+                        scope.launch {
+                            drawerState.close() // Uso correcto para cerrar el Drawer
+                        }
+                    }
+                )//end NavigationDrawerItem
+
+                //Chips
+                NavigationDrawerItem(
+                    label = {
+                        Text(text = "Chips")
+                    },
+                    selected = false,
+                    onClick = {
+                        component = "chips"
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    }
+                )//end NavigationDrawerItem
+
+            }//end ModelDrawerSheet
+        }//drawerContent
     ) {
+
         Column {
             when(component){//switchcasse
                 "Content1"->{
@@ -71,8 +123,15 @@ fun ComponentsScreen(navController: NavController) {
                 "Content2"->{
                     Content2()
                 }
+                "floating-buttons"->{
+                    FloatingButtons()
+                }
+                "chips"->{
+                    FloatingButtons()
+                }
             }
-        }
+        }//end Column
+
     }
 }
 
@@ -85,3 +144,111 @@ fun Content1() {
 fun Content2() {
     Text(text = "Content2")
 }
+@Composable
+fun Buttons(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Button(
+            onClick = {}
+        ){
+            Text(text = "Filled")
+        }
+
+        FilledTonalButton(
+            onClick = {}
+        ){
+            Text(text = "Tonal")
+        }
+
+        OutlinedButton(
+            onClick = {}
+        ){
+            Text(text = "Outlined")
+        }
+
+        ElevatedButton(
+            onClick = {}
+        ){
+            Text(text = "Elevated")
+        }
+
+        TextButton(
+            onClick = {}
+        ){
+            Text(text = "Text")
+        }
+
+    }
+}
+//@Preview(showBackground = true)
+@Composable
+fun FloatingButtons( ) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        FloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "")
+        }
+
+        SmallFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "")
+        }
+
+        LargeFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "")
+        }
+        ExtendedFloatingActionButton(
+            onClick = {},
+            icon =  {Icon(Icons.Filled.Add, "")},
+            text = {Text(text = "Extended FAB")}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Chips( ) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        AssistChip(
+            onClick = {},
+            label = { Text(text = "Assist Chip")},
+            leadingIcon = {
+                Icon(Icons.Filled.AccountBox, "",
+                    Modifier.size(AssistChipDefaults.IconSize))
+            }
+        )//end AssistChip
+
+        var selected by remember { mutableStateOf(false)}
+
+        FilterChip(
+
+            selected = selected,
+            onClick = {},
+            label = { Text(text = "Filter Chip")},
+            leadingIcon = if(selected){
+                {
+                    Icon(
+                        Icons.Filled.AccountBox, "",
+                        Modifier.size(AssistChipDefaults.IconSize)
+                    )//end Icon
+                }
+            }else{
+                null
+            }
+
+        )//end FilterChip
+
+    }
+}//end Chips
